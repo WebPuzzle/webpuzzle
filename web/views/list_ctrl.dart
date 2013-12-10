f41ebb2222d1a147b265ed1489563b067d0be14c
Michael Dawkins <michaeldawfr@gmail.com>
2013-12-10 20:15:47 +0100

    filter by name

--- web/views/list_ctrl.dart
+++ web/views/list_ctrl.dart
@@ -13,12 +13,31 @@ class ListCtrl {
   
   var filters = ['filtre 1', 'filtre 2'];
   
+  var nameFilter = '';
+  
   var sortingTypes = [
-    {'name'  : 'asc'},
-    {'name'  : 'desc'},
-    {'name'  : 'pop'},
-    {'name'  : 'pop inv'}
+  {
+    'name': 'Ascending alphabetical order',
+    'iconCss': 'fa fa-sort-alpha-asc',
+    'filter': 'name'
+  },
+  {
+    'name': 'Descending alphabetical order',
+    'iconCss': 'fa fa-sort-alpha-desc',
+    'filter': '-name'
+  },
+  {
+    'name': 'Descending popularity order',
+    'iconCss': 'fa fa-sort-amount-desc',
+    'filter': '-popularity'
+  },
+  {
+    'name': 'Ascending popularity order',
+    'iconCss': 'fa fa-sort-amount-asc',
+    'filter': 'popularity'
+  }
   ];
+
 
   ListCtrl(Router this.router, Http this._http){
     _http(method: 'GET', url: 'webcomponents.json').then((HttpResponse data) {
