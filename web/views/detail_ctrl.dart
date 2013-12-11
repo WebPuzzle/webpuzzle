45301e7e4182cab37c58d0e70a548de13c9d2afc
Michael Dawkins <michaeldawfr@gmail.com>
2013-12-11 21:42:57 +0100

    Detail view can get data if no selectedWC is found

--- web/views/detail_ctrl.dart
+++ web/views/detail_ctrl.dart
@@ -6,14 +6,22 @@ part of webpuzzle;
 )
 class DetailCtrl {
 
+  var currentId;
   var selectedWCService;
-  
   var selectedWC;
   
-  DetailCtrl (Scope scope, SelectedWC selectedWC){
-    this.selectedWCService = selectedWC;
-    this.selectedWC = selectedWCService.selectedWC;
-    print("selectedWC in found in detail is ${this.selectedWC}");
+  DetailCtrl (RouteProvider routeProvider ,SelectedWCService this.selectedWCService, Http http){
+    selectedWC = selectedWCService.selectedWC;
+    currentId = routeProvider.parameters["id"];
+    
+    if (selectedWC == null){
+      http.get('http://webpuzzlews.herokuapp.com/web_components/$currentId.json').then((HttpResponse response) {
+      selectedWCService.selectedWC = response.data;
+      selectedWC = response.data;
+      print("No selectedWC in memory, got data from WS");
+    });
+    }
   }
+  
 }
 
