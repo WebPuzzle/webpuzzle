7937289d924bad5d5bdaddf2030038055a134ed5
Michael Dawkins <michaeldawfr@gmail.com>
2013-12-11 09:58:31 +0100

    viewMode handling

--- web/views/list_ctrl.dart
+++ web/views/list_ctrl.dart
@@ -48,11 +48,22 @@ class ListCtrl {
      'filter': 'submitter'
    }
    ];
+  
+  var viewMode;
 
   ListCtrl(Router this.router, Http this._http){
     _http(method: 'GET', url: 'webcomponents.json').then((HttpResponse data) {
       webcomponents = data.data;
-      print(data);
     });
+    viewMode = router.activePath.last.name;
+    print("viewMode is now $viewMode");
+    router.onRouteStart.listen((RouteStartEvent rse) {
+      rse.completed.then((bool){
+        viewMode = router.activePath.last.name;
+        print("viewMode is now $viewMode");
+      });
+    });
+      
+    
   }
 }
