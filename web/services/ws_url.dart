276f9de920dbd17b850ecb3f94db29df24917a7f
Benoit Vasseur <benoit.vasseur14@gmail.com>
2014-01-16 09:36:02 +0100

    [Fix] change webServiceUrl for prod
    http://localhost:3000 to https://webpuzzlews.herokuapp.com

--- web/services/ws_url.dart
+++ web/services/ws_url.dart
@@ -8,7 +8,7 @@ class WsUrl {
     webServiceUrl = 'https://webpuzzlews.herokuapp.com';
     //Local configuration, removed at build time
     //@exclude
-    webServiceUrl = 'http://localhost:3000';
+    //webServiceUrl = 'http://localhost:3000';
     //@endexclude
     
   }
